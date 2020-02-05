2b21e1adaed52e350d1f9d40c77e12f9e2519d6c
Ian Kahwai <iankahwai@gmail.com>
2020-02-05 18:56:32 +0200

    Implementation of problem 3

--- problem_3/largest_prime_factor.py
+++ problem_3/largest_prime_factor.py
@@ -1,5 +1,27 @@
-def largestPrimeFactor(number):
-  
-  return true
+def largestPrimeFactor(value):
+  max_val = 0
+  prime = 2
 
-print(largestPrimeFactor(13195))
+  while value>1:
+    
+    if value % prime == 0:
+      value /= prime
+      max_val = prime
+      
+    else:
+      
+      while True:
+        is_prime = True
+        prime += 1
+        if prime % 2 == 0:
+            is_prime = False
+        
+          
+        if is_prime == True:
+          break
+      
+
+  
+  return max_val
+
+print(largestPrimeFactor(600851475143))

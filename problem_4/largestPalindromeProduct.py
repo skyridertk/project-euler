c0d550504f641d8ddcded59fffae8fd26075bfdf
Ian Kahwai <iankahwai@gmail.com>
2020-02-06 21:24:11 +0200

    Update progress of implementation of problem 4

--- problem_4/largestPalindromeProduct.py
+++ problem_4/largestPalindromeProduct.py
@@ -1,6 +1,32 @@
-def largestPalindromeProduct(n):
-  
-  return True
+from math import *
 
+def largestPalindromeProduct(n):
+    lower_limit = int(pow(10, n-1))
+    upper_limit = int(lower_limit * 10) - 1
+    max_result = 0
+
+    while upper_limit>=pow(10, n-1):
+        
+
+        for i in range(upper_limit+1, lower_limit-1, -1):
+
+            product = upper_limit * i
+            
+            if(product < max_result):
+                break
+            
+            
+            res = product == reverse(product)
+            
+            if(res and product > max_result):
+                max_result = product
+        
+            
+        upper_limit -= 1
+        
+    return max_result
+
+def reverse(num): 
+    return int(num != 0) and ((num % 10) * (10**int(log(num, 10))) + reverse(num // 10))
 
 print(largestPalindromeProduct(3))

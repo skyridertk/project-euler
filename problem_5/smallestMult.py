bfb7a7315bec32aec1213f94891fdbaa9b5a28d5
Ian Kahwai <iankahwai@gmail.com>
2020-02-07 16:20:00 +0200

    Add implementation to problem 5

--- problem_5/smallestMult.py
+++ problem_5/smallestMult.py
@@ -1,5 +1,17 @@
 def smallestMult(n):
-  
-  return True
+  value = 2
+  escape = False
+  while not escape:
+    
+    escape = True
+    for i in range(n, 1, -1):
+   
+      if value%i != 0:
+        escape = False
+        break
+    
+    value += 2
+    
+  return value-2
 
 print(smallestMult(20))

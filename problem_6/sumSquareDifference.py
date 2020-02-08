5cd5f12503232660d79f9ede70e145e8cfc6b12f
Ian Kahwai <iankahwai@gmail.com>
2020-02-08 23:21:55 +0200

    Add implementation of problem 6

--- problem_6/sumSquareDifference.py
+++ problem_6/sumSquareDifference.py
@@ -1,6 +1,11 @@
+from math import *
+
 def sumSquareDifference(n):
-  
-  return True
+  sum_values = pow(n*(n+1)/2, 2)
+
+  sum_squares_values = n*(n+1)*(2*n+1)/6
+
+  return sum_values - sum_squares_values
 
 
 print(sumSquareDifference(100))

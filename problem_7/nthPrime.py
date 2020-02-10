d87664af4d09f17b5fe2c04637148281aa6738e7
Ian Kahwai <iankahwai@gmail.com>
2020-02-10 05:22:09 +0200

    Add Implementation of problem 7

--- problem_7/nthPrime.py
+++ problem_7/nthPrime.py
@@ -1,4 +1,32 @@
 def nthPrime(n):
-    return True
+    current = 1
+    for i in range(n):
+        
+        current = findNextPrime(current)
+        
+    return current
+
+def findNextPrime(number):
+    number += 1
+    
+    while True:
+        is_prime = True
+
+        if((number % 2 == 0 or number % 3 == 0) and number >3):
+            is_prime = False
+            number += 1
+        else:
+
+            for i in range (2, number):
+                if number % i == 0:
+                    number += 1
+                    is_prime = False
+                    break
+            
+        if is_prime:
+            break
+
+    return number
+
 
 print(nthPrime(10001))

af978faf51afa1f31608e13799701419bf6e8405
Ian Kahwai <iankahwai@gmail.com>
2020-02-12 16:39:58 +0200

    Add implemenration of problem 10

--- problem_10/primeSummation.py
+++ problem_10/primeSummation.py
@@ -1,6 +1,39 @@
 def primeSummation(n):
+  sumOfPrime = 0
+  start = 1
   
-  return True
+  while start < n:
+    start = findNextPrime(start)
+    if(start < n):
+      sumOfPrime += start
+    else:
+      break
+
+    
+  
+  return sumOfPrime
 
 
-print(primeSummation(2000000))
+def findNextPrime(number):
+    number += 1
+    
+    while True:
+        is_prime = True
+
+        if((number % 2 == 0 or number % 3 == 0) and number >3):
+            is_prime = False
+            number += 1
+        else:
+
+            for i in range (2, number):
+                if number % i == 0:
+                    number += 1
+                    is_prime = False
+                    break
+            
+        if is_prime:
+            break
+
+    return number
+
+print(primeSummation(2001))

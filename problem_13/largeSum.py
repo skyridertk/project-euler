882a5971976634a60f1d2f0c155e409c8d6bb1b9
Ian Kahwai <iankahwai@gmail.com>
2020-02-16 22:41:30 +0200

    Initialize problem 13

---
+++ problem_13/largeSum.py
@@ -0,0 +1,13 @@
+def largeSum(arr):
+  
+  return True
+
+
+#  only change code above this line
+
+testNums = [
+  '37107287533902102798797998220837590246510135740250',
+  '46376937677490009712648124896970078050417018260538'
+]
+
+print(largeSum(testNums))

8e4b38311b721e884c6e08e2bec7380719d8afcf
Ian Kahwai <iankahwai@gmail.com>
2020-02-16 21:39:05 +0200

    Add implementation of problem 12

--- problem_12/divisibleTriangleNumber.py
+++ problem_12/divisibleTriangleNumber.py
@@ -1,5 +1,26 @@
 def divisibleTriangleNumber(n):
-  
-  return True
+  counter =1
+  triangle_num = 1
+  while True:
+    if(numberOfDivisors(triangle_num) >= n):
+      break
+    
+    counter+= 1
+    triangle_num += counter
 
-print(divisibleTriangleNumber(500))
+    
+
+  
+  return triangle_num
+
+def numberOfDivisors(value):
+  divisors = 0
+  if value == 1:
+    return 1
+  else:
+    for i in range(1, value+1):
+      if value % i == 0:
+        divisors += 1
+    return divisors
+
+print(divisibleTriangleNumber(167))

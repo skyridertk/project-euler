d690dd3565d3b128ab6c79b8c811ffda5f18cdd9
Ian Kahwai <iankahwai@gmail.com>
2020-02-19 20:13:08 +0200

    Add Implementation of problem 14

--- problem_14/longestCollatzSequence.py
+++ problem_14/longestCollatzSequence.py
@@ -1,6 +1,24 @@
 def longestCollatzSequence(limit):
-  
-  return True
+  occurrence = 0
+  num = 0
+
+  for i in range(2, limit+1):
+    temp = 0
+    numbr = i
+    while numbr>1:
+      temp+=1
+    
+      if(numbr%2 == 0):
+        numbr = numbr/2
+      else:
+        numbr = 3*numbr + 1
+
+    if(temp>=occurrence):
+      occurrence=temp
+      num = i
 
 
-print(longestCollatzSequence(14))
+  return num
+
+
+print(longestCollatzSequence(5847))

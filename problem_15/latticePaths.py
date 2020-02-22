2b855519a0e1c3e95eb2aa324d9c25d219ddc8be
Ian Kahwai <iankahwai@gmail.com>
2020-02-22 21:25:14 +0200

    Add implementation of problem 15

--- problem_15/latticePaths.py
+++ problem_15/latticePaths.py
@@ -1,75 +1,16 @@
+from math import factorial as fac
+#xCy = x! / (y! (x-y)!)
+def binomial(x, y):
+    try:
+        binom = fac(x) // fac(y) // fac(x - y)
+    except ValueError:
+        binom = 0
+    return binom
+
+
 def latticePaths(gridSize):
-  vertices = []
-  terminating_vertices = []
-  path = 0
-
-  for i in range(gridSize+1):
-    vertices.append([0, i])
-    terminating_vertices.append([i, 0])
-
-  for j in range(1, gridSize+1):
-    vertices.append([j, vertices[len(vertices)-1][1]])
-    terminating_vertices.append([vertices[len(vertices)-1][1], j])
-
-  path += 2
-
-  '''
-  break_point = False
-  stages = 0
-  while not break_point:
-    for val in range(len(vertices)):
-      if(vertices[val][0] == 1 and vertices[val+1][0] == 2):
-        print(vertices[val])
-      
-
-      if(vertices[val][0] == 2):
-        break
-    
-
-      #if #20 is reached break point True
-    
-    if(break_point == False):
-      stages += 1
-  '''
-  print(vertices)
-  print(terminating_vertices)
-  #00
-  #01
-  #02 x
-  #12
-  #22
-
-  #00
-  #01
-  #11
-  #12 x
-  #22
-
-  #Repeat
-  #00
-  #01 y
-  #11
-  #21
-  #22
-
-  #00
-  #10
-  #11
-  #12
-  #22
-
-  #00
-  #10
-  #11
-  #21
-  #22
-
-  #00
-  #10
-  #20
-  #21
-  #22
-  return True
+  
+  return binomial(2*gridSize, gridSize)
 
 
-print(latticePaths(3))
+print(latticePaths(9))

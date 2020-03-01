59f8edfcf38f62943dac487913ebb18fa536ed61
Ian Kahwai <iankahwai@gmail.com>
2020-03-01 14:50:43 +0200

    Update files

--- problem_17/numberLetterCounts.py
+++ problem_17/numberLetterCounts.py
@@ -2,6 +2,7 @@ def numberLetterCounts(val):
 	words = { 0: '', 1: 'one', 2: 'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine', 10:'ten', 11:'eleven', 12:'twelve', 13:'thirteen', 14:'fourteen', 15:'fifteen', 16:'sixteen', 17:'seventeen', 18:'eighteen', 19:'nineteenth', 20:'twenty', 30: 'thirty', 40:'forty', 50:'fifty', 60:'sixty', 70:'seventy', 80:'eighty', 90:'ninety', 100:'hundred', 1000: 'thousand'}
 	u_str=''
 	for limit in range(1, val+1):
+		original_value = limit
 		and_v = False
 		while True:
 			if limit >=1000:
@@ -27,7 +28,7 @@ def numberLetterCounts(val):
 				and_v = True
 			else:
 			
-				if and_v == False and limit > 0 and len(str(val)) > 1:
+				if and_v == False and limit > 0 and len(str(original_value)) > 1:
 					u_str += 'and'+words[limit]
 				else:
 					u_str += words[limit]
